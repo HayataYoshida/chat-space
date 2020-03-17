6399d7a37e29bd8b7c132bec5cd0dfdc89464434
HayataYoshida <hayatayoshida19940729@gmail.com>
2020-03-17 21:31:32 +0900

    自動更新機能

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -19,28 +19,36 @@ function buildHTML(message) {
 
   return html;       
 }
-$('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })
-.done(function(data){ 
-  var html = buildHTML(data);
-  $('.messages').append(html);
-    $("form")[0].reset();
-    $('input').prop('disabled', false);
-    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
-  })
-.fail(function() {
-  alert("メッセージ送信に失敗しました");
-  });
+
+
+
+$(function(){
+  var reloadMessages = function(){  //自動更新の関数
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){ //今いるページのリンクが/groups/グループid/messagesであれば以下を実行
+      var last_message_id = $('.message:last').data("message-id"); //dataメソッドで.messageにある:last(最後)のカスタムデータ属性を取得しlast_message_idに代入
+      $.ajax({
+        url: "api/messages", //サーバの指定(api/message_controller)
+        type: 'get', //メソッド(get)
+        dataType: 'json', //データの形式(json)
+        data: {id: last_message_id} //飛ばすデータ(取得したlast_message_id)
+      })
+      .done(function(messages){ //自動更新成功の時の処理(controllerから受け取ったmessageを引数にする)
+        var insertHTML = ''; //HTMLの入れ物
+        messages.forEach(function(message){ //配列messagesの中身一つ一つをHTMLに変換して入れ物に足し合わせる
+          insertHTML = buildHTML(message); //メッセージが入ったHTMLを取得 
+          $(".messages").append(insertHTML); //メッセージを追加
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 50); //自動更新が成功した時(メッセージが送信できた時)のみ一番下までスクロールする
+        })
+      })
+
+      .fail(function(){ //自動更新が失敗した時の処理
+        alert('自動更新に失敗しました'); //アラートを出す
+      });
+    }
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000); //7000ミリ秒(7秒)ごとにreloadMessages(自動更新の関数)を実行
+});
 });
 
 //id属性はブラウザの検証を利用するとわかる。 new_messageと書いてある。
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,16 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update]    # インクリメンタルサーチを実装の為、indexアクションを追記
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+
+    namespace :api do   #  namespace :ディレクトリ名 do ~ endと囲む形でルーティングを記述すると、そのディレクトリ内のコントローラのアクションを指定出来る。
+      resources :messages, only: :index, defaults: { format: 'json' }   # defaultsオプション = このルーティングが来たらjson形式でレスポンスするよう指定
+    end
   end
 end
+
+
+
+
+
+#自動生成されたルートを確認
+# $ rails routes
