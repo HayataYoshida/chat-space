0eb343e1e487deb0493454bced2061318faed536
HayataYoshida <hayatayoshida19940729@gmail.com>
2020-03-21 19:21:15 +0900

    自動更新機能

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,55 +1,98 @@
-function buildHTML(message) {
-  var image = message.image ?`<img src= "${message.image}">`:""; 
-  var html = `<div class ="message" data-message-id="${message.id}">
-                <div class ="upper-message">
-                  <div class ="upper-message__user-name">
+$(function(){
+
+  function buildHTML(message){
+    image = ( message.image ) ? `<img class="lower-message__image" src=${message.image} >` : ""; //三項演算子
+                var html =  
+                ` <div class="message" data-message-id="${message.id}">
+                <div class="upper-message">
+                  <div class="upper-message__user-name">
                     ${message.user_name}
                   </div>
-                  <div class ="upper-message__date">
+                  <div class="upper-message__date">
                     ${message.date}
                   </div>
                 </div>
-                <div class ="text-message">
-                  <p class ="text-message__content">
-                      ${content}
+                <div class="lower-message">
+                  <p class="lower-message__content">
+                  ${message.content}
                   </p>
-                      ${image}
+                  ${image}
                 </div>
               </div>`
-
-  return html;       
-}
-
+          return html; 
+  }
 
 
-$(function(){
-  var reloadMessages = function(){  //自動更新の関数
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){ //今いるページのリンクが/groups/グループid/messagesであれば以下を実行
-      var last_message_id = $('.message:last').data("message-id"); //dataメソッドで.messageにある:last(最後)のカスタムデータ属性を取得しlast_message_idに代入
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $("form")[0].reset();
+      $('input').prop('disabled', false);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 50);
+    })
+      .fail(function() {
+        alert("メッセージ送信に失敗しました");
+    });
+  });
+  
+
+
+　　　　　　//以下は自動更新の記述
+
+
+
+
+
+
+
+    var reloadMessages = function() {
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      var last_message_id = $('.message:last').data("message-id");
       $.ajax({
-        url: "api/messages", //サーバの指定(api/message_controller)
-        type: 'get', //メソッド(get)
-        dataType: 'json', //データの形式(json)
-        data: {id: last_message_id} //飛ばすデータ(取得したlast_message_id)
+        //ルーティングで設定した通りのURLを指定
+        url: "api/messages",
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
       })
-      .done(function(messages){ //自動更新成功の時の処理(controllerから受け取ったmessageを引数にする)
-        var insertHTML = ''; //HTMLの入れ物
-        messages.forEach(function(message){ //配列messagesの中身一つ一つをHTMLに変換して入れ物に足し合わせる
-          insertHTML = buildHTML(message); //メッセージが入ったHTMLを取得 
-          $(".messages").append(insertHTML); //メッセージを追加
-          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 50); //自動更新が成功した時(メッセージが送信できた時)のみ一番下までスクロールする
-        })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
       })
-
-      .fail(function(){ //自動更新が失敗した時の処理
-        alert('自動更新に失敗しました'); //アラートを出す
+      .fail(function() {
+        alert('error');
       });
-    }
-  };
+    };
+  
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 7000); //7000ミリ秒(7秒)ごとにreloadMessages(自動更新の関数)を実行
-});
-});
+    setInterval(reloadMessages, 7000);    // 引数で渡している7000という数字は、7秒という意味
+  }   
+
+  });
+
+
+
 
 //id属性はブラウザの検証を利用するとわかる。 new_messageと書いてある。
 // 「console.log()」は何らかの文字列を出力するためのメソッド
